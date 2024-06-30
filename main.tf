697819c214e10dda3b78409087db4a81c100a656
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-06-30 16:04:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,22 +18,3 @@ resource "aws_ec2_transit_gateway_route_table_association" "dx_gateway_lnd_eng_1
   transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_attachment.lnd_eng_1.transit_gateway_attachment_id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.main_router.id
 }
-
-data "aws_ec2_transit_gateway_attachment" "lnd_eng_1" {
-  filter {
-    name   = "resource-id"
-    values = [aws_dx_gateway_association.lnd_eng_1.id]
-  }
-}
-
-resource "aws_ec2_transit_gateway" "main_router" {
-  amazon_side_asn = "64512"
-}
-
-resource "aws_ec2_transit_gateway_route_table" "main_router" {
-  transit_gateway_id = aws_ec2_transit_gateway.main_router.id
-}
-
-locals {
-  dx_gateway_allowed_prefixes = ["10.0.0.0/16"]
-}

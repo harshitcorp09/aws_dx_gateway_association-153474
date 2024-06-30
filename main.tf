17a755a9b21ed535b54d2aeb5f11a9243a89eb03
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-06-30 16:10:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,27 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_dx_gateway" "lnd_eng_1" {
-  name            = "LDN_IXN_ENG_1"
-  amazon_side_asn = "6000"
+
+resource "aws_dx_gateway" "example" {
+  name            = "example"
+  amazon_side_asn = "64512"
 }
 
-resource "aws_dx_gateway_association" "lnd_eng_1" {
-  dx_gateway_id         = aws_dx_gateway.lnd_eng_1.id
-  associated_gateway_id = aws_ec2_transit_gateway.main_router.id
-
-  allowed_prefixes = local.dx_gateway_allowed_prefixes
+resource "aws_ec2_transit_gateway" "example" {
+  amazon_side_asn = "64512"
 }
 
-resource "aws_ec2_transit_gateway_route_table_association" "dx_gateway_lnd_eng_1" {
-  transit_gateway_attachment_id  = data.aws_ec2_transit_gateway_attachment.lnd_eng_1.transit_gateway_attachment_id
-  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.main_router.id
+resource "aws_ec2_transit_gateway_route_table" "example" {
+  transit_gateway_id = aws_ec2_transit_gateway.example.id
+}
+
+resource "aws_dx_gateway_association" "example" {
+  dx_gateway_id         = aws_dx_gateway.example.id
+  associated_gateway_id = aws_ec2_transit_gateway.example.id
+  allowed_prefixes      = ["10.0.0.0/16"]
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "example" {
+  transit_gateway_attachment_id  = aws_dx_gateway_association.example.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.example.id
 }

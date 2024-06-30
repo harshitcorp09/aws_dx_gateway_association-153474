c89c4f6ada03793ba259de9702af1b5059eee972
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-06-30 17:08:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_ec2_transit_gateway_route_table" "example" {
 resource "aws_dx_gateway_association" "example" {
   dx_gateway_id        = aws_dx_gateway.example.id
   allowed_prefixes     = ["10.0.0.0/16"]
-  transit_gateway_id   = aws_ec2_transit_gateway.example.id
+  associated_gateway_id   = aws_ec2_transit_gateway.example.id
 }
 
 resource "aws_ec2_transit_gateway_route_table_association" "example" {

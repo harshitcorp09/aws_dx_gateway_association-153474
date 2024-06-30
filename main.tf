deedf70d9e64724c2ce3572463b230d1ef8ff85f
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-06-30 16:08:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
 resource "aws_dx_gateway" "lnd_eng_1" {
   name            = "LDN_IXN_ENG_1"
-  amazon_side_asn = "64513"
+  amazon_side_asn = "6000"
 }
 
 resource "aws_dx_gateway_association" "lnd_eng_1" {
   dx_gateway_id         = aws_dx_gateway.lnd_eng_1.id
   associated_gateway_id = aws_ec2_transit_gateway.main_router.id
 
-  allowed_prefixes = ["10.0.0.0/16"]
+  allowed_prefixes = local.dx_gateway_allowed_prefixes
 }
 
 resource "aws_ec2_transit_gateway_route_table_association" "dx_gateway_lnd_eng_1" {

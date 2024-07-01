00128c793c83cb3e0e6bfdda91e39c27736ca8e5
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-07-01 11:20:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,10 +30,19 @@ resource "aws_dx_gateway_association" "example" {
   allowed_prefixes   = ["10.0.0.0/16"]  # Adjust according to your on-premises network prefixes
   associated_gateway_id = aws_ec2_transit_gateway.example.id
 }
+# Create a Transit Gateway VPC Attachment as a placeholder
+resource "aws_ec2_transit_gateway_vpc_attachment" "example" {
+  transit_gateway_id = aws_ec2_transit_gateway.example.id
+  vpc_id             = "vpc-065094bd3867b53af"
+  subnet_ids         = ["subnet-056001c77a8f9cbcd"]
 
+  tags = {
+    Name = "example-transit-gateway-vpc-attachment"
+  }
+}
 # Associate Transit Gateway with Route Table
 resource "aws_ec2_transit_gateway_route_table_association" "example" {
-  transit_gateway_attachment_id = aws_dx_gateway_association.example.id
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.example.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.example.id
 }
 
